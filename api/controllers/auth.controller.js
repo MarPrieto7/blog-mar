59f819187d713ca4414743b007082d6942d1fd36
MarPrieto7 <mmarprietogarcia@gmail.com>
2024-04-18 19:20:00 +0200

    Add admin functionality fo the user

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -18,6 +18,7 @@ export const signup = async (req, res, next) => {
     }
 
     const hashedPassword = bcryptjs.hashSync(password, 10);
+
       const newUser = new User({
         username,
         email,
@@ -32,7 +33,6 @@ export const signup = async (req, res, next) => {
       }
     };
     
-
     export const signin = async (req, res, next) => {
       const { email, password } = req.body;
     
@@ -72,23 +72,35 @@ export const signup = async (req, res, next) => {
       try {
           const user = await User.findOne({email});
           if (user) {
-              const token = jwt.sign({id: user._id, isAdmin: user.isAdmin}, process.env.JWT_SECRET);
+              const token = jwt.sign(
+                {id: user._id, isAdmin: user.isAdmin}, 
+                process.env.JWT_SECRET
+              );
               const {password, ...rest} = user._doc;
-              res.status(200).cookie('access_token', token, {
+              res
+              .status(200).cookie('access_token', token, {
                   httpOnly: true,
                   
-              }).json(rest); 
+              })
+              .json(rest); 
           }else{
-              const generatedPassword = Math.random().toString(36).slice(-8) + Math.random().toString(36).slice(-8);
+              const generatedPassword = 
+              Math.random().toString(36).slice(-8) + 
+              Math.random().toString(36).slice(-8);
               const hashedPassword = bcryptjs.hashSync(generatedPassword, 10);
               const newUser = new User({
-                  username: name.toLowerCase().split('').join('') + Math.random().toString(9).slice(-4),
+                  username: 
+                  name.toLowerCase().split('').join('') + 
+                  Math.random().toString(9).slice(-4),
                   email,
                   password: hashedPassword,
                   profilePicture: googlePhotoUrl,
               });
               await newUser.save();
-              const token = jwt.sign({ id: newUser._id, isAdmin:  newUser.isAdmin}, process.env.JWT_SECRET);
+              const token = jwt.sign(
+                { id: newUser._id, isAdmin:  newUser.isAdmin}, 
+                process.env.JWT_SECRET
+              );
               const {password, ...rest} = newUser._doc;
               res
               .status(200)
@@ -98,7 +110,7 @@ export const signup = async (req, res, next) => {
               .json(rest);
           }
       } catch (error) {
-          next(error)
-          
+          next(error) 
       }
-  }
+    };
+    
