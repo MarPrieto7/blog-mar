b7f4769bf201e191efb7fbcde9970581295ff37b
MarPrieto7 <mmarprietogarcia@gmail.com>
2024-04-18 14:44:18 +0200

    Complete  user image upload functionality

--- client/src/components/DashSidebar.jsx
+++ client/src/components/DashSidebar.jsx
@@ -1,10 +1,18 @@
-import { Sidebar } from 'flowbite-react'
-import {HiAnnotation, HiChartPie, HiDocumentText, HiOutlineUserGroup, HiUser, HiArrowSmRight} from 'react-icons/hi'
+import { Sidebar } from 'flowbite-react';
+import {
+  HiUser,
+  HiArrowSmRight,
+  HiDocumentText,
+  HiOutlineUserGroup,
+  HiAnnotation,
+  HiChartPie,
+} from 'react-icons/hi';
 import { useEffect, useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import { signoutSuccess } from '../redux/user/userSlice';
 import { useDispatch } from 'react-redux';
 import { useSelector } from 'react-redux';
+
 
 export default function DashSidebar() {
     const location = useLocation();
@@ -18,6 +26,7 @@ export default function DashSidebar() {
         setTab(tabFormUrl);
       }
     }, [location.search]);
+
     const handleSignout = async () =>{
       try {
         const res = await fetch('/api/user/signout', {
@@ -33,70 +42,75 @@ export default function DashSidebar() {
         console.log(error.message);
       }
     };
+
     return (
-      <Sidebar className='w-full md:w-56'>
-        <Sidebar.Items>
-          <Sidebar.ItemGroup className='flex flex-col gap-1'>
-            {currentUser && currentUser.isAdmin && (
-              <Link to='/dashboard?tab=dash'>
+    <Sidebar className='w-full md:w-56'>
+      <Sidebar.Items>
+        <Sidebar.ItemGroup className='flex flex-col gap-1'>
+          {currentUser && currentUser.isAdmin && (
+            <Link to='/dashboard?tab=dash'>
+              <Sidebar.Item
+                active={tab === 'dash' || !tab}
+                icon={HiChartPie}
+                as='div'
+              >
+                Dashboard
+              </Sidebar.Item>
+            </Link>
+          )}
+          <Link to='/dashboard?tab=profile'>
+            <Sidebar.Item
+              active={tab === 'profile'}
+              icon={HiUser}
+              label={currentUser.isAdmin ? 'Admin' : 'User'}
+              labelColor='dark'
+              as='div'
+            >
+              Profile
+            </Sidebar.Item>
+          </Link>
+          {currentUser.isAdmin && (
+            <Link to='/dashboard?tab=posts'>
+              <Sidebar.Item
+                active={tab === 'posts'}
+                icon={HiDocumentText}
+                as='div'
+              >
+                Posts
+              </Sidebar.Item>
+            </Link>
+          )}
+          {currentUser.isAdmin && (
+            <>
+              <Link to='/dashboard?tab=users'>
                 <Sidebar.Item
-                  active={tab === 'dash' || !tab}
-                  icon={HiChartPie}
+                  active={tab === 'users'}
+                  icon={HiOutlineUserGroup}
                   as='div'
                 >
-                  Dashboard
+                  Users
                 </Sidebar.Item>
               </Link>
-            )}
-                <Link to='/dashboard?tab=profile'>
-                <Sidebar.Item 
-                active={tab === 'profile'}
-                icon={HiUser}
-                label={currentUser.isAdmin ? 'Admin' : 'User'} 
-                labelColor='dark'
-                as='div'
+              <Link to='/dashboard?tab=comments'>
+                <Sidebar.Item
+                  active={tab === 'comments'}
+                  icon={HiAnnotation}
+                  as='div'
                 >
-                Profile
+                  Comments
                 </Sidebar.Item>
-                </Link>
-                {currentUser.isAdmin && (
-                <Link to='/dashboard?tab=post'>
-                  <Sidebar.Item
-                    active={tab === 'post'}
-                    icon={HiDocumentText}
-                    as='div'
-                  >
-                    Posts
-                  </Sidebar.Item>
-                </Link>
-                )}
-                {currentUser.isAdmin && (
-                  <>
-                  <Link to='/dashboard?tab=users'>
-                  <Sidebar.Item
-                    active={tab === 'users'}
-                    icon={HiOutlineUserGroup}
-                    as='div'
-                  >
-                    Users
-                  </Sidebar.Item>
-                </Link>
-                  <Link to='/dashboard?tab=comments'>
-                  <Sidebar.Item
-                    active={tab === 'users'}
-                    icon={HiAnnotation}
-                    as='div'
-                  >
-                    Comments
-                  </Sidebar.Item>
-                </Link>
-                  </>
-                )}
-                <Sidebar.Item icon={HiArrowSmRight} className='cursor-pointer' onClick={handleSignout} >
-                    Sign Out
-                </Sidebar.Item>
-            </Sidebar.ItemGroup>
-        </Sidebar.Items>
+              </Link>
+            </>
+          )}
+          <Sidebar.Item
+            icon={HiArrowSmRight}
+            className='cursor-pointer'
+            onClick={handleSignout}
+          >
+            Sign Out
+          </Sidebar.Item>
+        </Sidebar.ItemGroup>
+      </Sidebar.Items>
     </Sidebar>
-  )
+  );
 }
