09aa32bfa951509483015d4e07dad51ee75a58e1
MarPrieto7 <mmarprietogarcia@gmail.com>
2024-04-21 17:56:26 +0200

    import getUsers in folder user.route.js

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import { test } from '../controllers/user.controller.js';
-import { updateUser, deleteUser, signout} from '../controllers/user.controller.js';
+import {getUsers,updateUser, deleteUser, signout} from '../controllers/user.controller.js';
 import { verifyToken } from '../utils/verifyUser.js';
 
 const router = express.Router();
