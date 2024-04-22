ee1e0758008cdf4dc17a3fc40fc31ddfd068f501
MarPrieto7 <mmarprietogarcia@gmail.com>
2024-04-22 20:17:13 +0200

    Show the comments of  post

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import { test } from '../controllers/user.controller.js';
-import {getUsers,updateUser, deleteUser, signout} from '../controllers/user.controller.js';
+import {getUsers,getUser, updateUser, deleteUser, signout} from '../controllers/user.controller.js';
 import { verifyToken } from '../utils/verifyUser.js';
 
 
@@ -11,5 +11,6 @@ router.put('/update/:userId', verifyToken, updateUser);
 router.delete('/delete/:userId', verifyToken, deleteUser);
 router.post('/signout', signout);
 router.get('/getusers', verifyToken, getUsers);
+router.get('/:userId', getUser);
 
 export default router;
--- client/src/components/CommentSection.jsx
+++ client/src/components/CommentSection.jsx
@@ -110,6 +110,7 @@ export default function CommentSection({ postId }) {
       console.log(error.message);
     }
   };
+  
   return (
     <div className='max-w-2xl mx-auto w-full p-3'>
       {currentUser ? (
