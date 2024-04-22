d23fb9eefcbbce6b69e7b85a1b9280981d6bd308
MarPrieto7 <mmarprietogarcia@gmail.com>
2024-04-22 19:58:46 +0200

    Add comment section to the post page

--- api/index.js
+++ api/index.js
@@ -4,7 +4,7 @@ import dotenv from 'dotenv';
 import userRoutes from './routes/user.route.js';
 import authRoutes from './routes/auth.route.js';
 import postRoutes from './routes/post.route.js';
-// import commentRoutes from './routes/comment.route.js';
+import commentRoutes from './routes/comment.route.js';
 import cookieParser from 'cookie-parser';
 import path from 'path';
 
@@ -33,7 +33,7 @@ app.listen(3000, () => {
 app.use('/api/user', userRoutes);
 app.use('/api/auth', authRoutes);
 app.use('/api/post', postRoutes);
-// app.use('/api/comment', commentRoutes);
+app.use('/api/comment', commentRoutes);
 
 app.use(express.static(path.join(__dirname, '/client/dist')));
 
@@ -50,3 +50,4 @@ app.use((err, req, res, next) => {
     message,
   });
 });
+
--- client/src/pages/PostPage.jsx
+++ client/src/pages/PostPage.jsx
@@ -82,7 +82,7 @@ export default function PostPage() {
           {post && (post.content.length / 1000).toFixed(0)} mins read
         </span>
       </div>
-      
+
       <div
         className='p-3 max-w-2xl mx-auto w-full post-content'
         dangerouslySetInnerHTML={{ __html: post && post.content }}
@@ -90,7 +90,7 @@ export default function PostPage() {
       <div className='max-w-4xl mx-auto w-full'>
         <CallToAction />
       </div>
-      <CommentSection postId={post._id} />
+      <CommentSection postId={post._id} /> 
 
       <div className='flex flex-col justify-center items-center mb-5'>
         <h1 className='text-xl mt-5'>Recent articles</h1>
