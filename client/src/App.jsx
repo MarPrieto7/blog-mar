c033c3d93c555ed8a71079c43f18a33f5fc44638
MarPrieto7 <mmarprietogarcia@gmail.com>
2024-04-22 19:09:33 +0200

    Complete post page functionality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,36 +11,30 @@ import PrivateRoute from './components/PrivateRoute';
 import OnlyAdminPrivateRoute from './components/OnlyAdminPrivateRoute';
 import CreatePost from './pages/CreatePost';
 import UpdatePost from './pages/UpdatePost';
-
-
-
-
-
+import PostPage from './pages/PostPage';
 
 export default function App() {
-  return (  
+  return (
     <BrowserRouter>
-        
-         <Header />
+      <Header />
       <Routes>
-        <Route index element={<Home />} />
+        <Route path='/' element={<Home />} />
         <Route path='/about' element={<About />} />
         <Route path='/sign-in' element={<SignIn />} />
         <Route path='/sign-up' element={<SignUp />} />
+        <Route path='/search' element={<Search />} />
         <Route element={<PrivateRoute />}>
           <Route path='/dashboard' element={<Dashboard />} />
         </Route>
         <Route element={<OnlyAdminPrivateRoute />}>
           <Route path='/create-post' element={<CreatePost />} />
           <Route path='/update-post/:postId' element={<UpdatePost />} />
-
-
         </Route>
 
-        < Route path = '/projects' element={<Projects />}/>
-
+        <Route path='/projects' element={<Projects />} />
+        <Route path='/post/:postSlug' element={<PostPage />} />
       </Routes>
-        <Footer />
+      <Footer />
     </BrowserRouter>
   );
 }
