4fbba88ca7164c8d22f8e751371769c83a89e134
MarPrieto7 <mmarprietogarcia@gmail.com>
2024-04-23 15:24:29 +0200

    add dashboard overview to the admin dashboard

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -5,7 +5,7 @@ import DashProfile from '../components/DashProfile';
 import DashPosts from '../components/DashPosts';
 import DashUsers from '../components/DashUsers';
 import DashComments from '../components/DashComments';
-// import DashboardComp from '../components/DashboardComp';
+import DashboardComp from '../components/DashboardComp';
 
 export default function Dashboard() {
   const location = useLocation();
@@ -32,7 +32,7 @@ export default function Dashboard() {
        {/* comments  */}
        {tab === 'comments' && <DashComments />}
       {/* dashboard comp */}
-      {/* {tab === 'dash' && <DashboardComp />}  */}
+      {tab === 'dash' && <DashboardComp />} 
     </div>
   );
 }
