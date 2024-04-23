df7219c86035f0d86669bfae0299011938a7a7fa
MarPrieto7 <mmarprietogarcia@gmail.com>
2024-04-23 15:14:17 +0200

    Add import DashComments to Dashboard

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -4,7 +4,7 @@ import DashSidebar from '../components/DashSidebar';
 import DashProfile from '../components/DashProfile';
 import DashPosts from '../components/DashPosts';
 import DashUsers from '../components/DashUsers';
-// import DashComments from '../components/DashComments';
+import DashComments from '../components/DashComments';
 // import DashboardComp from '../components/DashboardComp';
 
 export default function Dashboard() {
