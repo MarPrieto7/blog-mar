61ef73530e2f32cd9426c71455b801329bdd7a90
MarPrieto7 <mmarprietogarcia@gmail.com>
2024-04-23 19:28:08 +0200

    Complete about pages

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
-
 export default function About() {
   return (
-    <div>
-      About
+    <div className='min-h-screen flex items-center justify-center'>
+      <div className='max-w-2xl mx-auto p-3 text-center'>
+        <div>
+          <h1 className='text-3xl font font-semibold text-center my-7'>
+            About Mar con Art
+          </h1>
+          <div className='text-md text-gray-500 flex flex-col gap-6'>
+            <p>
+            Welcome to Mar con Art. This blog was created by Mar Prieto as a personal project to share her paintings, artwork, ideas, and tips with the world. The idea was to unite two of my passions, coding and creating magic with art, a developer who is starting in the world of programming and with a future vision of transforming a painting into code.
+            </p>
+
+            <p>
+            In this blog, you will find articles and tutorials on topics such as pigments, brushes, types of paints, and materials used in the creation of a painting. Mar is always creating, learning, and exploring new ways of making pictorial art, so be sure to check back often for new content!
+            </p>
+
+            <p>
+            We encourage you to leave comments on our posts and engage with other readers. You can like other people's comments and respond to them as well. We believe that a community of people who enjoy art can help improve and grow together.
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
