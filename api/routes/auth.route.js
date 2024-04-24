4ab7fcbf5b1dbf16fd961321814948793c385f6e
MarPrieto7 <mmarprietogarcia@gmail.com>
2024-04-24 17:20:23 +0200

    Update auth.route.js

--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -2,7 +2,7 @@ import express from 'express';
 import { signup } from '../controllers/auth.controller.js';
 import { signin } from '../controllers/auth.controller.js';
 import { google } from '../controllers/auth.controller.js';
-import { limitLogin } from "../middlewares/timeOut.js";
+import { limitLogin } from "../middlewares/timeout.js";
 
 const router = express.Router();
 
