2c8daf7401310bd08b67e359a12a59e779946734
MarPrieto7 <mmarprietogarcia@gmail.com>
2024-04-24 13:48:12 +0200

    Add rate limit in SignIn from backend

---
+++ api/middlewares/timeout.js
@@ -0,0 +1,8 @@
+import { rateLimit } from 'express-rate-limit'
+
+//Uso la libreria rateLimit para definir la cantidad de intentos que se pueden hacer en el login
+export const limitLogin = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 min
+    max: 3, // Max number of entries to try log in
+    message: 'Too many fail requests, try in again in 15 minutes',
+})
--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -2,12 +2,13 @@ import express from 'express';
 import { signup } from '../controllers/auth.controller.js';
 import { signin } from '../controllers/auth.controller.js';
 import { google } from '../controllers/auth.controller.js';
+import { limitLogin } from "../middlewares/timeOut.js";
 
 const router = express.Router();
 
 
 router.post('/signup', signup);
-router.post('/signin', signin);
+router.post('/signin', limitLogin, signin);
 router.post('/google', google);
 
 export default router;
